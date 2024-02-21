cccf85a73b6990249ddd1f6bf997a26876aed597
simopucci <simo.pucci7@gmail.com>
2024-02-21 13:24:31 +0100

    feat: add newsletter

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
 
   export default {
     data() {
@@ -68,7 +69,7 @@
       };
     },
 
-    components: { AppHeader, AppMain }
+    components: { AppHeader, AppMain, AppFooter }
   }
 
 </script>
@@ -79,8 +80,8 @@
   :logoPath="headerItems.logo"
   :socials="headerItems.socials">
   </AppHeader>
-  
   <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,52 @@
+<script>
+    export default {
+        
+    }
+</script>
+
+<template>
+    <footer>
+        <div class="newsletter rounded d-flex">
+            <img src="../assets/img/news.png" alt="">
+            <div class="d-flex justify-content-center align-items-center flex-column">
+                <h5 class="text-white">Subscribe to Our Newsletter</h5>
+                <div class="input-group flex-nowrap email">
+                    <input type="text" class="form-control" placeholder="Your Email Here">
+                    <span class="input-group-text" id="addon-wrapping">@</span>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+
+<style lang="scss" scoped>
+footer {
+    background-color: black;
+    height: 400px;
+    position: relative;
+
+    .newsletter {
+        position: relative;
+        background-color: #CE7C2A;
+        top: -80px;
+        height: 150px;
+        width: 600px;
+        margin: 0 auto;
+
+        .email {
+            width: 300px;
+        }
+
+        img {
+            width: 240px;
+            height: 210px;
+            position: relative;
+            top: -60px;
+            margin-right: 50px;
+        }
+    }
+
+    
+}
+</style>
